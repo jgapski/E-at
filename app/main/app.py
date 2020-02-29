2d52b1db9fb37c883938f60570447da0d6613d8a
osman efe <efeergun96@gmail.com>
2020-02-29 17:33:51 +0100

    prediciton path

--- app/main/app.py
+++ app/main/app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+import cv2
+import os
+from food_classifier import FoodClassifier
+from food_analiser import FoodAnaliser
 
 app = Flask(__name__)
 
@@ -8,5 +12,15 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/food_prediction')
+def predict_food():
+    image = cv2.imread("C:/Users/Efe/Documents/GitHub/E-at/app/test_images/salad_test.jpg")   ## this is a placeholder hardcode
+    food_clf = FoodClassifier()
+    prediction = food_clf.predict(image)
+
+    fa = FoodAnaliser()
+    return fa.get_food_nutritions(prediction)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)

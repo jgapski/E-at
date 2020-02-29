451f2f3ff21ca5fd4dc428a3781104de47d5c88b
osman efe <efeergun96@gmail.com>
2020-02-29 17:58:20 +0100

    update_prediciton

--- app/main/app.py
+++ app/main/app.py
@@ -1,8 +1,5 @@
 from flask import Flask
 import cv2
-import os
-from food_classifier import FoodClassifier
-from food_analiser import FoodAnaliser
 import config
 import pymongo
 from flask import Flask, jsonify
@@ -85,16 +82,6 @@ def post_photo():
         return {"result": str(error)}, 400
     except Exception as err:
         return {"result": str(err)}, 500
-
-
-@app.route('/food_prediction')
-def predict_food():
-    image = cv2.imread("C:/Users/Efe/Documents/GitHub/E-at/app/test_images/salad_test.jpg")   ## this is a placeholder hardcode
-    food_clf = FoodClassifier()
-    prediction = food_clf.predict(image)
-
-    fa = FoodAnaliser()
-    return fa.get_food_nutritions(prediction)
 
 
 if __name__ == '__main__':
